a0c9706ffe40901bade412960fc06ae69c22eac0
unknown <pinguh@naver.com>
2021-09-05 13:55:35 +0900

    0905

---
+++ js/background.js
@@ -0,0 +1,11 @@
+const images = [
+    "1.jpg", "2.jpg", "3.jpg", "4.jpg"
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+const bgimage = document.createElement("img");
+
+bgimage.src = `img/${chosenImage}`;
+document.body.appendChild(bgimage);
+
+console.log(bgimage);
---
+++ js/todo.js
@@ -0,0 +1,50 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event){
+ const li = event.target.parentElement;
+ li.remove();
+}
+
+function paintToDo(newToDo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newToDo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    toDos.push(newTodo);
+    paintToDo(newTodo);
+    saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+function sayHello(item){
+    console.log("hello");
+}
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.array.forEach(sayHello);
+}
